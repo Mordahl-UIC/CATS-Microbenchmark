b6708458b539cff1d90c3c46660627564538ae0e
Austin Mordahl <amordahl@uic.edu>
2026-06-29 14:42:43 -0500

    Revert "Add package names"
    This reverts commit 2b968a28c6a0f6e80c823784e7190e80d351f98a.

--- benchmarks/Serialization/SerializationAndLambdas/SerLam2/src/main/java/cats/serlam2/serlam/Test.java
+++
@@ -1,8 +0,0 @@
-// serlam/Test.java
-package cats.serlam2.serlam;
-
-import java.io.Serializable;
-
-public @FunctionalInterface interface Test extends Serializable{
-    String concat(Integer seconds);
-}
--- benchmarks/TypeCasts/TC2/src/main/java/cats/tc2/castclassapi/Target.java
+++
@@ -1,9 +0,0 @@
-// castclassapi/Target.java
-package cats.tc2.castclassapi;
-
-public class Target {
-
-    public String toString(){
-        return "Target";
-    }
-}
